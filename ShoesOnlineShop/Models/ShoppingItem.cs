16d7c55afabb50d2d263b444d583e9f585d321af
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2022-11-24 12:52:30 +0000

    Added ShoppingItem to model

---
+++ ShoesOnlineShop/Models/ShoppingItem.cs
@@ -0,0 +1,13 @@
+﻿namespace ShoesOnlineShop.Models
+{
+    public class ShoppingItem
+    {
+        public int Id { get; set; }
+        
+        public Product Product { get; set; }
+
+        public int Quantity { get; set; } = 1;
+
+        public decimal PurachasePrice { get; set; } = 0;
+    }
+}

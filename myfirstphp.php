eb3eccd03a1678181dc358e8d5717b672197743b
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-03-04 22:35:56 +0000

    addred
    hello
    
    Co-Authored-By: Hamza Emre <hamzaemre0@gmail.com>

--- myfirstphp.php
+++ myfirstphp.php
@@ -23,9 +23,6 @@
 	print($myage);
 ?>	
 <?php
-    include("myfunctions.inc.php");
-    html_header("My second function demo");
-	html_h1("Tax Calculation");
-    echo "I pay £" .calculatetax(15000,22,10000) . "tax";
+    
     html_footer();
 ?>

be1b19537964f40267ae1b18cfaff9aee9d5181d
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-03-04 22:00:45 +0000

    Update console.js
    added

--- console.js
+++ console.js
@@ -1,3 +1,14 @@
 //comment
 console.login("hello world")
 console.login("hello world")// second line
+
+console.login("hello world")// second line
+
+
+onsole.login("hello world")// second line
+
+onsole.login("hello world")// second line
+onsole.login("hello world")// second line
+
+print csreen().
+

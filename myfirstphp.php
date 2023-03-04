caf0b4ae504ea7d8477d3cba600d8d8cacb21fda
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-03-04 22:22:48 +0000

    Update myfirstphp.php

--- myfirstphp.php
+++ myfirstphp.php
@@ -11,3 +11,14 @@
 	<p> My gross wage is <?php print("$gross"); ?>
 </body>
 </html>
+// commente
+<?php 
+	$myage = "None of your business <br/>";
+	print($myage);
+	
+	$myage  = 21;
+    print($myage. "<br/>");
+    
+	$myage = 21.75;
+	print($myage);
+?>	

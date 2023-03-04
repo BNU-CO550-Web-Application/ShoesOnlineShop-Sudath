bf9adabfbb0f70a042b4470570e788cafc29a7b7
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-03-04 22:13:10 +0000

    adsdsffdf adee

--- hello.php
+++ hello.php
@@ -3,4 +3,9 @@
     $hourlyrate	= 5.75;
 	$hoursperweek = 40;
 	$gross = $hourlyrate * $hoursperweek;
+?>///hello	
+<?php 
+    $hourlyrate	= 5.75;
+	$hoursperweek = 40;
+	$gross = $hourlyrate * $hoursperweek;
 ?>	

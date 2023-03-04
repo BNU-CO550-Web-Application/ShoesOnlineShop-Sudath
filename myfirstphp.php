ff63d3ee32e8cfef441210615a794e199b953bc6
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-03-04 22:20:25 +0000

    udped m

--- myfirstphp.php
+++ myfirstphp.php
@@ -4,3 +4,10 @@
 	$hoursperweek = 40;
 	$gross = $hourlyrate * $hoursperweek;
 ?>	
+<-- Html code --->
+<html>
+<head></head>
+<body>
+	<p> My gross wage is <?php print("$gross"); ?>
+</body>
+</html>

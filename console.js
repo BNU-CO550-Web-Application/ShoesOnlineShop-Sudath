a5968b016958f7c24cae259de3354827a591662d
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-03-04 22:04:05 +0000

    Update console.js
    added

--- console.js
+++ console.js
@@ -1,3 +1,12 @@
 //comment
 console.login("hello world")
 console.login("hello world")// second line
+console.login("hello world")// second line
+
+
+console.login("hello world")// second line
+console.login("hello world")// second line
+
+fdfdf
+
+print lin();
